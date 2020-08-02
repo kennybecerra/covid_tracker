40f0f5e64816e1c8817f1974a3244082d581cf68
Kenny Becerra <KennyBecerra.j@gmail.com>
2020-08-01 22:36:20 -0700

    Updates

---
+++ precache-manifest.08aa9ee46f895ebf0b9f29ca334e4587.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307a527b5e981e910fa4564f147901d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06ae7ab97b361f94b60a",
+    "url": "/static/css/main.50074652.chunk.css"
+  },
+  {
+    "revision": "05abe4b71c113aa4ce81",
+    "url": "/static/js/2.4ba47229.chunk.js"
+  },
+  {
+    "revision": "9e899dfd7ae2e062da19e83984708bc5",
+    "url": "/static/js/2.4ba47229.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06ae7ab97b361f94b60a",
+    "url": "/static/js/main.7d10adb2.chunk.js"
+  },
+  {
+    "revision": "0f141f1a15ea4bbbb834",
+    "url": "/static/js/runtime-main.ca98175e.js"
+  }
+]);
