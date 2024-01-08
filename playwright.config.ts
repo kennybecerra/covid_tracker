5a4e1333152ce72ba9e71896090646b943ba771c
Kenny Becerra <kennybecerra@ken-pro.local>
2024-01-08 13:09:04 -0800

    adding playwright github actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,8 +20,8 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  // reporter: "line",
-  reporter: [["junit", { outputFile: "results.xml" }]],
+  reporter: "html",
+  // reporter: [["junit", { outputFile: "results.xml" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -72,9 +72,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "npm run start",
+    url: "http://localhost:4040",
+    reuseExistingServer: false,
+    timeout: 120 * 1000,
+  },
 });
